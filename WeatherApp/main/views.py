bd189671306d49514d7cd2a17c796606a42a9184
abhishek173 <abhishekkumar801104@gmail.com>
2024-09-13 22:05:04 +0530

    projcet completed successfully

---
+++ WeatherApp/main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import urllib.request
+import json 
+
+def index(request):
+    if request.method == "POST":
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f95a76cef12bcf5ed1e16b293e366803').read()
+
+        list_of_data = json.loads(source)
+        # data for variable list_of_data 
+        data = { 
+            "country_code": str(list_of_data['sys']['country']), 
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']), 
+            "temp": str(list_of_data['main']['temp']) + 'k', 
+            "pressure": str(list_of_data['main']['pressure']), 
+            "humidity": str(list_of_data['main']['humidity']), 
+        } 
+        print(data)
+    else:
+        data = {}
+
+    return render(request,'main/index.html',data)
+
+
+
